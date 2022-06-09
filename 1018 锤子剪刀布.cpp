b8ef2a32d664cdf8e4945e65f9ee43487a9f350e
U2yyy <a1535887812@gmail.com>
2022-06-09 12:28:59 +0800

    完善了‘石头剪刀布’的代码，现在可以完全通过了

--- 1018 锤子剪刀布.cpp
+++ 1018 锤子剪刀布.cpp
@@ -20,13 +20,15 @@ char max(struct peo *date){
 	char B = 'B';
 	char J = 'J';
 	char C = 'C';
-	if(date->numB == date->numC)
-		C = B;
-	if(date->numB == date->numJ)
-		J = B;	
-	int maxnum = date->numB > date->numC ? date->numB : date->numC;
-	char max = date->numB > date->numC ? B : C;
-	max = maxnum > date->numJ ? max : J;
+	//我超有猪，这么写的话万一把比较小的两个量换成B了咋办，真的是猪，想的又复杂又漏洞百出 
+//	if(date->numB == date->numC)
+//		C = B;
+//	if(date->numB == date->numJ)
+//		J = B;
+	//把下面的>改成>=不就可以了吗，真的是难绷 
+	int maxnum = date->numB >= date->numC ? date->numB : date->numC;
+	char max = date->numB >= date->numC ? B : C;
+	max = maxnum >= date->numJ ? max : J;
 	return max;
 }
 int main(){
