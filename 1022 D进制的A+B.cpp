ae311245c06b6949adb6e6143f7bd0b751aeb4fa
U2yyy <a1535887812@gmail.com>
2022-06-14 13:42:49 +0800

    修改了1022 D进制的A+B

--- 1022 D进制的A+B.cpp
+++ 1022 D进制的A+B.cpp
@@ -6,6 +6,11 @@ int main(){
 	vector<int>outcome;
 	cin>>A>>B>>D;
 	int n = A + B;
+	//改进后发现是n=0时输出不正确 
+	if(n == 0){
+		cout<<"0";
+		return 0;
+	}
 	for(;n>0;){
 		outcome.push_back(n%D);
 		n /= D;
