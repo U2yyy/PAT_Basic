ebb38cc63288a83a5a9302edcff33b932137a08d
U2yyy <a1535887812@gmail.com>
2022-06-23 17:34:07 +0800

    更新了旧键盘，使用更为清晰简洁的代码实现了操作，也使得代码现在能完全通过oj了

--- 1029 旧键盘.cpp
+++ 1029 旧键盘.cpp
@@ -1,53 +1,40 @@
 #include<iostream>
+#include<string>
+#include<vector>
 using namespace std;
-void addfortimes(int *a,char *b){
-	for(int i=0;b[i]!='\0';i++){
-		a[b[i]]=1;
-	} 
-}
-void judge(int *a,char *b,int *c){
-	int k = 0;
-	for(int i=48;i<126;i++){
-		if(a[i]==1){
-			bool flag = true;
-			for(int j=0;b[j]!='\0';j++){
-				if(b[j]==i){
-					flag = false;
+int main(){
+	vector<char> broken;
+	vector<char>::const_iterator it; 
+	string s1,s2;
+	cin>>s1>>s2;
+	//先把所有字母都变成大写可以极大简化过程 
+	for(int i=0;i<s1.size();i++){
+		s1[i] = toupper(s1[i]);
+	}
+	for(int i=0;i<s2.size();i++){
+		s2[i] = toupper(s2[i]);
+	}
+	for(int i=0;i<s1.size();i++){
+		bool flag = false;
+		for(int j=0;j<s2.size();j++){
+			if(s1[i]==s2[j]){//这里是如果两个字符串中有相等情况就不压入栈中 
+				flag = true;
+				break;
+			}
+		}
+		if(flag == false){
+			bool anoflag = false;
+			for(it=broken.begin();it!=broken.end();it++){
+				if(s1[i]==*it){//anoflag是用来判断栈中之前的元素是不是已经存在这个即将压入的元素，若已经存在就不压入了 
+					anoflag = true;
 					break;
 				}
 			}
-			if(flag==true)
-				c[k++] = i;
-		}
-	} 
-}
-void judgeagain(int *a){
-		//如果大小写字母同时出现，应当只保留大写字母，若小写字母出现，则变为大写字母 
-	for(int i=0;i<80;i++){
-		if(a[i]!=0&&a[i]>='a'){
-			for(int j=0;j<80;j++){
-				if(a[j]==a[i]-32){
-					a[i] = 0;
-				}
-			}
-			a[i] -= 32;	
+			if(anoflag == false)
+				broken.push_back(s1[i]);
 		}
 	}
-} 
-int main(){
-	//定义整型数组用来存字符出现次数 
-	int times[126] = {0};
-	//定义一个整型数组用来存最终坏的字符的ASCII码 
-	int arr[80] = {0}; 
-	char oricha[81];
-	char cha[81];
-	cin>>oricha>>cha;
-	addfortimes(times,oricha);
-	judge(times,cha,arr);
-	judgeagain(arr);
-	for(int i=0;i<80;i++){
-		if(arr[i]>=0)
-			cout<<char(arr[i]);
+	for(it=broken.begin();it!=broken.end();it++){
+		cout<<*it; 
 	}
-	return 0;
 } 
