61065bb973797c7e067dc1136bd61fc806fe6645
U2yyy <a1535887812@gmail.com>
2022-07-15 17:47:06 +0800

    这个题目是一个数学问题

--- 1049 数列的片段和.cpp
+++ 1049 数列的片段和.cpp
@@ -1,36 +1,38 @@
 #include<iostream>
 //这个题本质上是个数学问题，记录一下我犯的蠢，数数字个数都没数对，导致找规律没找出来 
 using namespace std;
-double calculate(double a[],int n){
-	double sum = 0;
-	for(int i=0;i<n;i++){
-		sum += a[i];
+/*****************************************
+long long fun(double a[],int n){
+	/*************************
+	double cal;
+	int cnt = 1;
+	for(int i=1;i<n;i++){
+		cnt *= 2;
 	}
-	return sum;
-}
-double fun(double a[],int n){
-	double cal = 0;
 	for(int i=0;i<n;i++){
-		cal += a[i];
+		cal += a[i] * cnt;
 	}
-	for(int j=0;j<n;j++){
-		double *b = a;
-		for(int i=j;i<n;i++){
-			b[i] = 0;
-			cal += calculate(b,n);
-		}
+	如果题目没有要求要连续，那么可以使用这个解法 
+	//因为题目要求是连续，我们通过数学找规律可以找出来这个关系
+	//同时因为双精度有精度方面的缺陷，因此我们使用longlong类型先进行存储 
+	long long cal = 0;
+	for(int i=0;i<n;i++){
+		cal += (long long)(a[i]*1000)*(n-i+1)*i; 
 	}
 	return cal;
-} 
+******************************************/
 int main(){
 	int n;
 	cin>>n;
 	double sum;
-	double a[n];
+	//这里换成long double以避免精度不够 
+	long double temp;
+	long long biggerone = 0;
 	for(int i=0;i<n;i++){
-		cin>>a[i];
+		cin>>temp;
+		biggerone += (temp*1000)*(n-i)*(i+1); 
 	}
-	sum = fun(a,n);
+	sum = (double)biggerone/1000;
 	printf("%.2f",sum);
 	return 0;
 }
